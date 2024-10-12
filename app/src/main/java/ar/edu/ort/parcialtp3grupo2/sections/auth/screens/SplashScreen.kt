a34fc2bdb9884cdd5d95944c84fbeae80d9b008c
MartinSalama <martinsalama1@gmail.com>
2024-10-12 16:01:12 -0300

    pantallas auth

---
+++ app/src/main/java/ar/edu/ort/parcialtp3grupo2/sections/auth/screens/SplashScreen.kt
@@ -0,0 +1,104 @@
+package ar.edu.ort.parcialtp3grupo2.sections.auth.screens
+
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.delay
+
+import ar.edu.ort.parcialtp3grupo2.R
+
+@Composable
+fun SplashScreen(/*navController: NavController*/) {
+
+    val scaleAnimation: Animatable<Float, AnimationVector1D> = remember { Animatable(initialValue = 0f) }
+
+    AnimationSplashContent(
+        scaleAnimation = scaleAnimation,
+        navController = rememberNavController(),
+        durationMillisAnimation = 1500,
+        delayScreen = 3000L
+    )
+
+    DesignSplashScreen(
+        imagePainter = painterResource(id = R.drawable.logo_splash_screen),
+        scaleAnimation = scaleAnimation
+    )
+}
+
+@Composable
+fun AnimationSplashContent(
+    scaleAnimation: Animatable<Float, AnimationVector1D>,
+    navController: NavController,
+    durationMillisAnimation: Int,
+    delayScreen: Long
+) {
+
+    LaunchedEffect(key1 = true) {
+        scaleAnimation.animateTo(
+            targetValue = 0.5F,
+            animationSpec = tween(
+                durationMillis = durationMillisAnimation,
+                easing = {
+                    OvershootInterpolator(3F).getInterpolation(it)
+                }
+            )
+        )
+
+        delay(timeMillis = delayScreen)
+
+      /*  navController.navigate(route = DestinationScreen.MainScreenDest.route) {
+            popUpTo(route = DestinationScreen.SplashScreenDest.route) {
+                inclusive = true
+            }
+        }*/
+    }
+}
+
+@Composable
+fun DesignSplashScreen(
+    modifier: Modifier = Modifier,
+    imagePainter: Painter,
+    scaleAnimation: Animatable<Float, AnimationVector1D>
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFF53B175)),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = imagePainter,
+                contentDescription = "Logotipo Splash Screen",
+                modifier = modifier
+                    .size(400.dp)
+                    .scale(scale = scaleAnimation.value),
+            )
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Preview(){
+    SplashScreen()
+}
+

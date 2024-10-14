d336737b5fff99aaf2dac4e1836d4b0c93b2164e
CristianGGS <cristiangregores@hotmail.com>
2024-10-14 17:26:30 -0300

    favourites

--- app/src/main/java/ar/edu/ort/parcialtp3grupo2/sections/home/screens/MyCart.kt
+++ app/src/main/java/ar/edu/ort/parcialtp3grupo2/sections/home/screens/MyCart.kt
@@ -1,13 +1,20 @@
 package ar.edu.ort.parcialtp3grupo2.sections.home.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Divider
@@ -23,6 +30,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -89,40 +97,49 @@ fun MyCartContent(modifier: Modifier = Modifier) {
         }
     }
 }
-
-
-
 @Composable
 fun ProductRow(product: Product) {
-    Column(
+    Row(
         modifier = Modifier
-            .width(375.17.dp)
+            .width(375.dp)
             .height(96.98.dp)
-            .padding(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .padding(20.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-
-        Text(
-            text = product.name,
-            style = TextStyle(
-                fontSize = 16.sp,
-                color = Color(0xFF181725),
-                letterSpacing = 0.01.sp,
-                lineHeight = 18.sp
-            ),
-            modifier = Modifier.padding(bottom = 4.dp)
+        Image(
+            painter = painterResource(id = product.image),
+            contentDescription = product.name,
+            modifier = Modifier
+                .size(55.69.dp)
+                .padding(end = 8.dp)
         )
-
-        Text(
-            text = "${product.amount}, Price",
-            style = TextStyle(
-                fontSize = 14.sp,
-                color = Color(0xFF7C7C7C),
-                lineHeight = 18.sp,
-                letterSpacing = 0.sp
-            ),
-            modifier = Modifier.padding(bottom = 4.dp)
-        )
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = product.name,
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    color = Color(0xFF181725),
+                    letterSpacing = 0.01.sp,
+                    lineHeight = 18.sp
+                ),
+                modifier = Modifier.align(Alignment.Start)
+            )
+            Text(
+                text = "${product.amount}, Price",
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    color = Color(0xFF7C7C7C),
+                    lineHeight = 18.sp,
+                    letterSpacing = 0.sp
+                ),
+                modifier = Modifier.align(Alignment.Start)
+            )
+        }
         Text(
             text = "$${product.price}",
             style = TextStyle(
@@ -132,10 +149,10 @@ fun ProductRow(product: Product) {
                 lineHeight = 27.sp
             ),
             modifier = Modifier
-                .width(49.38.dp)
-                .height(27.dp)
-                .padding(top = 4.dp)
-                .align(Alignment.End)
+                .align(Alignment.CenterVertically)
+                .padding(start = 16.dp)
+                .width(IntrinsicSize.Max)
+                .wrapContentWidth(Alignment.End)
         )
     }
 }
